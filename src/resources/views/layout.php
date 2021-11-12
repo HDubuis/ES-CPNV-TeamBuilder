f34c7bc14bd87408f6f2453f10a3454f1bd9fdfe
HDubuis <helene.dubuis@cpnv.ch>
2021-11-12 09:59:57 +0100

    Start of exam

--- src/resources/views/layout.php
+++ src/resources/views/layout.php
@@ -11,7 +11,7 @@ use App\Controllers\Components\NavbarController,
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Teambuilder</title>
-
+    Version examen Dubuis
     <link rel="stylesheet" href="/css/app.css">
     <script type="text/javascript" src="/js/darkmode.js" defer></script>
 </head>
